308e35ecc1f3ce998eea6fd95b2c6e1ba90938ce
Alexander Tsvetkov <alexander@tsvetkov.io>
2021-04-13 13:21:17 +0200

    initial spring-initialized version

---
+++ src/main/kotlin/com/example/gnirps/GnirpsApplication.kt
@@ -0,0 +1,11 @@
+package com.example.gnirps
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class GnirpsApplication
+
+fun main(args: Array<String>) {
+	runApplication<GnirpsApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/gnirps/GnirpsApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.gnirps
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class GnirpsApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
