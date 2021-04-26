6e150422144f07d791e3d088cb44b487e1464902
Alexander Tsvetkov <alexander@tsvetkov.io>
2021-04-26 13:47:55 +0200

    - rest controller added - dockerfile added - sample gradle build step added

--- src/main/kotlin/com/example/gnirps/GnirpsApplication.kt
+++ src/main/kotlin/com/example/gnirps/GnirpsApplication.kt
@@ -2,10 +2,20 @@ package com.example.gnirps
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
 
 @SpringBootApplication
 class GnirpsApplication
 
 fun main(args: Array<String>) {
 	runApplication<GnirpsApplication>(*args)
+}
+
+@RestController
+@RequestMapping("/")
+class TestController {
+	@GetMapping("test")
+	fun test() = "hello there"
 }
